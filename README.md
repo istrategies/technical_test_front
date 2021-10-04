993991877942ba82720fad8fe057834c8928806a
RMT-Inn <91734888+RMT-Inn@users.noreply.github.com>
2021-10-04 12:58:22 +0200

    Repository initialization

---
+++ README.md
@@ -0,0 +1,17 @@
+# Innovation Strategies
+## _Front-End Technical Test_
+
+__You must use the main.ts file, using the foo function__
+
+1. For the first test, we ask you to create an object that contains a name and use a hello function that prints "hello {the person's name}"
+
+2. For the second part of the test, you have to make the above object immutable, and you have to create a test with Jest that validates that the object is immutable
+
+3. For this third part, you have to import the data.json file found in the root of src
+    - First you have to create an array of objects with the data obtained in the .json file.
+    - Now we must return the number of times the names are repeated with the following structure { name: string; times: number; }.
+    - According to the previous structure, it orders from highest to lowest, showing only the first 3, and only returning the name of the person.
+    - Create a test that checks all of part 3.
+
+## _Requirements for the test_
+  - Only have node installed
---
+++ src/main.ts
@@ -0,0 +1,49 @@
+export const foo = async (): Promise<void> => {
+  //#region PART 1
+  /**
+   * For the first test, we ask you to create an object that
+   * contains a name and use a hello function that prints "hello {the person's name}"
+   */
+
+  //TODO -->
+  //#endregion
+
+  //#region PART 2
+  /**
+   * For the second part of the test, you have to make the above object immutable,
+   * and you have to create a test with Jest that validates that the object is immutable
+   * **The test must be in a separate file**
+   */
+
+  //TODO -->
+  //#endregion
+
+  //#region  PART 3
+  // For this third part, you have to import the data.json file found in the root of src,
+
+  /**
+   * First you have to create an array of objects with the data obtained in the .json file
+   */
+
+  //TODO -->
+
+  /**
+   * Now we must return the number of times the names are repeated with the following structure
+   * { name: string; times: number; }
+   */
+
+  //TODO -->
+
+  /**
+   * According to the previous structure, it orders from highest to lowest,
+   * showing only the first 3, and only returning the name of the person
+   * **The number of names to display must be dynamic**
+   */
+  //TODO -->
+
+  /**
+   * Create a test that checks all of part 3
+   * **The test must be in a separate file**
+   */
+  //#endregion
+}
